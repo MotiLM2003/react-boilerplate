19b761c860cf6792fb6295e09fa43389316d49a2
Moti Elmakies <motiphone2003@gmail.com>
2020-10-25 14:12:25 +0200

    small package that contains react + babel boilerplate

---
+++ public/scripts/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('Working  :atm:');
+var testTemplate = React.createElement(
+  'div',
+  null,
+  'Hello World'
+);
+
+ReactDOM.render(testTemplate, document.getElementById('root'));
---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.log('Working  :atm:');
+const testTemplate = <div>Hello World</div>;
+
+ReactDOM.render(testTemplate, document.getElementById('root'));
